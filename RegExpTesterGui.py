f77d682c5d7b2a3846f4ec00af075c4e1c08bf57
David Calvin <davidc@dkc.(none)>
2019-06-23 05:24:27 -0700

    gui is now functional

--- RegExpTesterGui.py
+++ RegExpTesterGui.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 
 import sys
+import re
 from PyQt4.QtGui import *
-from PyQt4 import QtCore, QtGui
+from PyQt4 import QtCore as qt_core
+from PyQt4 import QtGui 
 from form import Ui_MainWindow
 
 class RegExpTestGui(QtGui.QMainWindow):
@@ -17,12 +19,60 @@ class RegExpTestGui(QtGui.QMainWindow):
 		regexp_pattern = self.ui.regex_line_edit_.text()
 		check_text = self.ui.check_text_line_edit_.text()
 		if regexp_pattern.isEmpty() :
-			self.ui.result_text_browser_.setText("RegExp empty")
+			message_list = qt_core.QStringList()
+			message_list << "<font color=\"red\">Failed</font>"
+			message_list << "RegExp field is empty."
+			message_list << "Nothing to do."
+			self.ui.result_text_browser_.setHtml(message_list.join("<br>"))
 		elif check_text.isEmpty() :
-			self.ui.result_text_browser_.setText("Check text empty")
+			message_list = qt_core.QStringList()
+			message_list << "<font color=\"red\">Failed</font>"
+			message_list << "CheckText field is empty."
+			message_list << "Nothing to do."
+			self.ui.result_text_browser_.setHtml(message_list.join("<br>"))
 		else :
 			self.ui.result_text_browser_.setText("Ready to go")
-			
+			regexp = re.compile(str(regexp_pattern))
+			full_matches = qt_core.QStringList()
+			captures = qt_core.QStringList()
+			pos = 0
+			while True :
+				match = regexp.match(check_text, pos)
+				if match is None :
+					break
+				pos = match.end(0) + 1
+				full_matches << match.group(0)
+				for capture in match.groups(0) :
+					captures << capture
+
+			if full_matches.count() == 0 :
+				message_list = qt_core.QStringList()
+				message_list << "<font color=\"red\">Failed</font>"
+				message_list << "RegExp does not match CheckText"
+				self.ui.result_text_browser_.setHtml(message_list.join("<br>"))
+			else :
+				message_list = qt_core.QStringList()
+				message_list << "<font color=\"green\">Success</font>"
+				message_list << "RegExp matches CheckText"
+
+				if full_matches.count() is 1 :
+					message_list << qt_core.QString("There is 1 full text match")
+				else :
+					message_list << qt_core.QString("There are %1 full text matches").arg(full_matches.count())
+				
+				message_list << "\"" + full_matches.join("\"<br>\"") + "\"<br>"
+				
+
+				if full_matches.count() is 0 :
+					message_list << qt_core.QString("There are no captures")
+				elif full_matches.count() is 1 :
+					message_list << qt_core.QString("There is 1 capture")
+				else :
+					message_list << qt_core.QString("There are %1 captures").arg(captures.count())
+				
+				message_list << "\"" + captures.join("\"<br>\"") + "\"<br>"
+
+				self.ui.result_text_browser_.setHtml(message_list.join("<br>"))
 
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
