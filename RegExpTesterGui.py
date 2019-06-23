e9fdcd98dabe25e313f6fb49da484f5803e26641
David Calvin <davidc@dkc.(none)>
2019-06-23 03:53:09 -0700

    setup if else branches

--- RegExpTesterGui.py
+++ RegExpTesterGui.py
@@ -13,7 +13,16 @@ class RegExpTestGui(QtGui.QMainWindow):
 		self.ui.run_button_.clicked.connect(self.loadRegExp)
 
 	def loadRegExp(self):
-		self.ui.result_text_browser_.setText("Yeah\nClicked")
+		#self.ui.result_text_browser_.setText("Yeah\nClicked")
+		regexp_pattern = self.ui.regex_line_edit_.text()
+		check_text = self.ui.check_text_line_edit_.text()
+		if regexp_pattern.isEmpty() :
+			self.ui.result_text_browser_.setText("RegExp empty")
+		elif check_text.isEmpty() :
+			self.ui.result_text_browser_.setText("Check text empty")
+		else :
+			self.ui.result_text_browser_.setText("Ready to go")
+			
 
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
